9d166bd5cd2c238d90745e2edf1cb0157e4816f5
moadhamousti <moadhamousti@gmail.com>
2024-08-10 22:15:56 +0100

    Update

--- server/index.js
+++ server/index.js
@@ -13,20 +13,32 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch((err) => console.log('Database not connected', err));
 
 // Middlewares
-app.use(express.json({ limit: '50mb' })); // Increase payload limit
-app.use(express.urlencoded({ limit: '50mb', extended: true })); // Increase payload limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
-// Configure CORS middleware
+// Define allowed origins
+const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2];
+const devOrigin = ['http://localhost:5173'];
+const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigin;
+
+// Configure CORS middleware to accept multiple origins
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with the origin of your frontend application
-    credentials: true // Allow credentials
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Allow credentials
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/courses', require('./routes/coursesRoute'));
-app.use('/api/form', require('./routes/formRoutes')); // Make sure this matches your API calls
+app.use('/api/form', require('./routes/formRoutes'));
 app.use('/', require('./routes/dashboardRoutes'));
 app.use('/api', require('./routes/userRoutes'));
 
